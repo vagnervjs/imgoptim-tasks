7f80da02cdc35629c75052929869a5583a8147f4
vagnervjs <vagnervjs@gmail.com>
2014-05-29 11:39:24 -0300

    First commit

---
+++ Gruntfile.js
@@ -0,0 +1,42 @@
+module.exports = function(grunt) {
+	'use strict';
+	var gruntConfig = {
+		pkg: grunt.file.readJSON('package.json'),
+        sprite:{
+            all: {
+                algorithm: 'left-right',
+                src: 'src/images/sprite/*',
+                destImg: 'src/images/sprite.png',
+                destCSS: 'src/less/sprite.less',
+                cssFormat: 'less',
+            }
+        },
+        imageoptim: {
+            dist: {
+                options: {
+                    jpegMini: false,
+                    imageAlpha: true,
+                    quitAfter: true
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'src/images',
+                    src: '**/*.{jpg,jpeg,png,gif}',
+                    dest: 'src/images'
+                }]
+            }
+        }
+	};
+	grunt.initConfig(gruntConfig);
+
+	var keys = Object.keys(gruntConfig);
+	var tasks = [];
+
+	for(var i = 1, l = keys.length; i < l; i++) {
+		tasks.push(keys[i]);
+	}
+
+    grunt.loadNpmTasks('grunt-spritesmith');
+	grunt.loadNpmTasks('grunt-imageoptim');
+	grunt.registerTask('default', tasks);
+};
---
+++ README.md
@@ -0,0 +1,42 @@
+# ImgOptim Tasks
+
+A base to create sprites and optimize images using Grunt
+
+
+## Getting Started
+
+- First, you need to have [Node.JS](http://nodejs.org/) and NPM installed.
+
+- Clone
+
+```bash
+$ git clone https://github.com/vagnervjs/imgoptim-tasks.git
+```
+
+- Enter in the folder
+
+```bash
+$ cd imgoptim-taks
+```
+
+- Install dependencies
+
+```bash
+$ sudo npm install
+```
+- Put your images inside src/images (or src/imges/sprites)
+
+- Just run
+
+```bash
+$ sudo grunt
+```
+
+## License
+
+Code is under [MIT license](http://vagnersantana.mit-license.org) © Vagner Santana.
+
+
+
+
+
